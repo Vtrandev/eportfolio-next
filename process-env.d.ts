5af7bc53ab03714d3e5edc112ddab47b79047799
Vuong Tran <121814098+Vtrandev@users.noreply.github.com>
2023-10-10 12:00:53 -0700

    Emailjs environmental variables working as intended

--- process-env.d.ts
+++ process-env.d.ts
@@ -1,11 +1,10 @@
-
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-    //   [key: string]: string | undefined;
-      EMAILSERVICE_ID: string;
-      EMAILTEMPLATE_ID: string;
-      EMAILPUBLIC_ID: string;
+      //   [key: string]: string | undefined;
+      NEXT_PUBLIC_EMAILSERVICE_ID: string;
+      NEXT_PUBLIC_EMAILTEMPLATE_ID: string;
+      NEXT_PUBLIC_EMAILPUBLIC_ID: string;
       // add more environment variables and their types here
     }
   }
